061c1c74cd6caa878bc2c9cb7b166409b6d6eb0c
rickyricky787 <rrodriguez2017@gmail.com>
2020-07-28 15:34:03 -0400

    Second commit

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request
-from animescorescraper import MyAnimeList, AnimePlanet, AniList
+from .animescorescraper import MyAnimeList, AnimePlanet, AniList
 
 app = Flask(__name__)
 
