f11ee55820f6e4187e4cc78c16bb347fc2a07221
rickyricky787 <rrodriguez2017@gmail.com>
2020-10-04 20:15:30 -0400

    Fixes for heroku

--- app/main.py
+++ app/main.py
@@ -4,29 +4,26 @@ from .animescorescraper import *
 
 cache = Cache()
 
-def create_app():
-    app = Flask(__name__)
-    app.config['CACHE_TYPE'] = 'simple'
-    cache.init_app(app)
+app = Flask(__name__)
+app.config['CACHE_TYPE'] = 'simple'
+cache.init_app(app)
 
-    @app.route("/", methods=["POST", "GET"])
-    def home():
-        if request.method == "POST":
-            query = request.form["nm"]
-            return redirect(url_for("results", qry=query))
-        else:
-            return render_template("home.html")
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if request.method == "POST":
+        query = request.form["nm"]
+        return redirect(url_for("results", qry=query))
+    else:
+        return render_template("home.html")
 
-    @app.route("/<qry>")
-    @cache.cached(timeout=120)
-    def results(qry):
-        search1 = MyAnimeListData(MyAnimeListLink(qry))
-        search2 = AnimePlanetData(AnimePlanetLink(qry))
-        search3 = AniListData(AniListLink(qry))
-        if request.method == "POST":
-            query = request.form["nm"]
-            return redirect(url_for("results", qry=query))
-        else:
-            return render_template("results.html", title=qry, anime1=search1, anime2=search2, anime3=search3)
-    
-    return app
+@app.route("/<qry>")
+@cache.cached(timeout=120)
+def results(qry):
+    search1 = MyAnimeListData(MyAnimeListLink(qry))
+    search2 = AnimePlanetData(AnimePlanetLink(qry))
+    search3 = AniListData(AniListLink(qry))
+    if request.method == "POST":
+        query = request.form["nm"]
+        return redirect(url_for("results", qry=query))
+    else:
+        return render_template("results.html", title=qry, anime1=search1, anime2=search2, anime3=search3)
